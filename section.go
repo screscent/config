c18e5c655e02ad9ff5f89b829aff9c7256905906
screscent <gonghaohua1985@163.com>
2013-06-09 12:49:00 +0800

    Create section.go

---
+++ section.go
@@ -0,0 +1,20 @@
+package config
+
+func (self *Config) AddSection(section string) bool {
+  if section == "" {
+		return false
+	}
+
+	if _, ok := self.data[section]; ok {
+		return false
+	}
+
+	self.data[section] = make(map[string]*TValue)
+	return true
+}
+
+func (self *Config) HasSection(section string) bool {
+	_, ok := self.data[section]
+
+	return ok
+}
